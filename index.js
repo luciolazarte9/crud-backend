b18bc0168bd86f47ef2f1df0a5f88f2e5307d9f3
Lucio Lazarte <lucioagustinlazarte@gmail.com>
2023-07-12 16:51:13 -0300

    added port in index config

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import express from 'express'
 
-console.log('first test backend')
+//tomar un puerto
+//crear una instancia de express
+const app = express();
+//crear variable para guardar un puerto 
+app.set('port', process.env.PORT || 4000);
+app.listen( app.get('port'), () =>{
+    console.log('Estoy en el puerto ' + app.get('port'))
+})
+//middleware
+
+//rutas
