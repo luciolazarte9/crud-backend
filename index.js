6e5578a1cb22abed6ca4be1630a353d209b8450d
Lucio Lazarte <lucioagustinlazarte@gmail.com>
2023-07-12 17:52:06 -0300

    conection to db

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import path from 'path'
+import 'dotenv/config'
+import './src/database/dbConection'
 
 //tomar un puerto
 //crear una instancia de express
@@ -16,7 +18,7 @@ app.use(cors()); // permite conexiones remotas
 app.use(express.json()); // puedo tomar del objeto request datos en formato json.
 app.use(morgan('dev')); //muestra informacion extra de las solicitudes get, post, put, etc.
 console.log(path.join(__dirname, '/public'));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public'))); //nos premite poder ejecutar los archivos estaticos de mi proyecto en la ruta raiz de mi backend ej: localhost:4000
 
 
 //rutas
